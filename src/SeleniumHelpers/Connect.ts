68df87945cfb775410442a0f6a281941b97b3654
MaksymW <mwerema@concisesoftware.com>
2023-08-09 09:27:34 +0200

    Mess

--- src/SeleniumHelpers/Connect.ts
+++ src/SeleniumHelpers/Connect.ts
@@ -1,65 +1,29 @@
-import { ConnectL } from "../SeleniumLocators/ConnectL.js";
-import { Apollo } from "../dataHelpers/getlist.js";
-import { waitForElement } from "./htmlHelper.js";
-import dotenv from 'dotenv';
-export async function Connect(driver: any, url: string, mess: string): Promise<void> {
+import { WebDriver } from 'selenium-webdriver';
+import { waitForElement } from './htmlHelper.js';
+import { sm } from '../SeleniumLocators/messL.js';
+    
+export async function sendMessage(driver: WebDriver,url:string,messagetit: string,message: string) {
+    try{
+        await driver.get(`${url}`);
+        const icon = await waitForElement(driver, sm.icon, 2000)
+        const lockicon = await icon.getAttribute('type');
+        console.log(lockicon)
+        if(lockicon !== 'locked') {
+            const mess = await waitForElement(driver, sm.mess, 2000);
+    
+            await mess.click();
+            const title = await waitForElement(driver, sm.title, 2000);
+            await title.click();
+            await title.sendKeys(messagetit);
+            
+            const messcontent = await waitForElement(driver, sm.messcontent, 2000);
+            await messcontent.click();
+            await messcontent.sendKeys(message);
+            // const send = await waitForElement(driver,sm.send,1000)
+            // await send.click()
 
-  await driver.get(`${url}`);
-
-  try {
-      const button = await waitForElement(driver, ConnectL.button, 2000);
-      const buttonText = await button.getText();
-      console.log(buttonText,"buttontext")
-      if (buttonText === "Connect") {
-          await button.click();
-          const messagebutton = await waitForElement(driver, ConnectL.messageButton, 10000);
-          messagebutton.click();
-
-          const messageInput = await waitForElement(driver, ConnectL.messageInput, 2000);
-          await messageInput.sendKeys(mess);
-
-          const sendButton = await waitForElement(driver, ConnectL.sendButton, 10000);
-          
-          await sendButton.click()
-          await driver.navigate().refresh();
-          console.log("Connect successful")
-          return;
-      } else {
-          const dropdown = await waitForElement(driver, ConnectL.dropdown, 2000);
-          await dropdown.click();
-          const connect = await waitForElement(driver, ConnectL.connect, 2000);
-          await connect.click();
-          
-          const messagebutton = await waitForElement(driver, ConnectL.messageButton, 10000);
-          await messagebutton.click();
-
-          const messageInput = await waitForElement(driver, ConnectL.messageInput, 2000);
-          await messageInput.sendKeys(mess);
-          const sendButton = await waitForElement(driver, ConnectL.sendButton, 10000);
-          await sendButton.click()
-          
-          await driver.navigate().refresh();
-          console.log("Connect successful")
-      }
-
-  } catch (error) {
-      console.log('Cannot find connect button');
-  }
-}
-
-
-export async function getmess(): Promise<string> {
-
-dotenv.config();
-const AppoloApi = process.env.API_KEY
-const apollo = new Apollo(AppoloApi);
-var lastTask = await apollo.GetLastask();
-const mess = `Hi ${lastTask.contact.first_name},
-It might be your lucky day to grow your tech team in ${lastTask.contact.organization_name}.
-We help companies like ${lastTask.contact.organization_name} to scale up and down their software teams quickly.
-Let me know if this is something we can help you with.
-
-Best,
-Kacper`
-return mess
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
